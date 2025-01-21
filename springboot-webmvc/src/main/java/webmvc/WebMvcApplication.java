622b5582f8fc7fcc517040c5991c9f39e9248bdb
Godbell <12191579@inha.edu>
2025-01-21 11:46:25 +0900

    Learn: auto config

---
+++ springboot-webmvc/src/main/java/webmvc/WebMvcApplication.java
@@ -0,0 +1,28 @@
+package webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class WebMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcApplication.class, args);
+    }
+
+    @Controller
+    public static class MvcController {
+        @ResponseBody
+        @GetMapping("/hello")
+        public String hello() {
+            return "Hello, World!";
+        }
+
+        @GetMapping("/hello2")
+        public String hello2() {
+            return "hello";
+        }
+    }
+}
